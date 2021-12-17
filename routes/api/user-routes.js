13c4f88c2b63f1e3d9e69b6ba66e5e523ec5684e
Sophia Barrett <sophia@yoursummit.media>
2021-12-16 20:06:19 -0700

    add api routes for users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,11 +2,35 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 router.get("/", (req, res) => {
-  User.findAll();
+  User.findAll()
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
-  User.findOne();
+  User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbData) => {
+      if (!dbData) {
+        res
+          .status(404)
+          .json({ message: `No user found with id ${req.params.id}` });
+      } else {
+        res.json(dbData);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
@@ -20,6 +44,25 @@ router.post("/", (req, res) => {
     favorite_genre_id: req.body.favorite_genre_id,
   })
     .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbData) => {
+      if (!dbData) {
+        res.status(404).json({ message: "No user found with this id." });
+        return;
+      }
       res.json(dbData);
     })
     .catch((err) => {
