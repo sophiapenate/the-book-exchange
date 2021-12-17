deafdd73ecdd5808580aa5586a1247b5793db872
Sophia Barrett <sophia@yoursummit.media>
2021-12-16 19:58:36 -0700

    add create user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,30 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 router.get("/", (req, res) => {
-    User.findAll();
+  User.findAll();
+});
+
+router.get("/:id", (req, res) => {
+  User.findOne();
+});
+
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    phone: req.body.phone,
+    favorite_genre_id: req.body.favorite_genre_id,
+  })
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
