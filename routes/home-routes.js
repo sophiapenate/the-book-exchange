66e830356e17319579b218d45f54ebc0de8d459e
Sophia Barrett <sophia@yoursummit.media>
2021-12-20 09:39:09 -0700

    restore homepage.handlebars and finalize search route

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -10,55 +10,62 @@ router.get("/search", async (req, res) => {
   const query = req.query.q;
   const queryArr = query.split(" ");
 
-  // search authors
-  const authorSearchResult = await Author.findAll({
-    where: {
-      [Op.or]: [
-        { first_name: { [Op.in]: queryArr } },
-        { last_name: { [Op.in]: queryArr } },
-        { first_name: { [Op.like]: `%${query}%` } },
-        { last_name: { [Op.like]: `%${query}%` } },
-      ],
-    },
-  });
+  try {
+    // search authors
+    const authorSearchResult = await Author.findAll({
+      where: {
+        [Op.or]: [
+          { first_name: { [Op.in]: queryArr } },
+          { last_name: { [Op.in]: queryArr } },
+          { first_name: { [Op.like]: `%${query}%` } },
+          { last_name: { [Op.like]: `%${query}%` } },
+        ],
+      },
+    });
 
-  // serialize author search data and get author ids
-  const authorIdsArr = authorSearchResult.map(
-    (author) => author.get({ plain: true }).id
-  );
+    // serialize author search data and get author ids
+    const authorIdsArr = authorSearchResult.map(
+      (author) => author.get({ plain: true }).id
+    );
 
-  // search genres
-  const genreSearchResult = await Genre.findAll({
-    where: {
-      [Op.or]: [
-        //{ name: { [Op.in]: queryArr } },
-        { name: { [Op.like]: `%${query}%` } },
-      ],
-    },
-  });
+    // search genres
+    const genreSearchResult = await Genre.findAll({
+      where: {
+        [Op.or]: [
+          //{ name: { [Op.in]: queryArr } },
+          { name: { [Op.like]: `%${query}%` } },
+        ],
+      },
+    });
 
-  // serialize genre search data and get genre ids
-  const genreIdsArr = genreSearchResult.map(
-    (genre) => genre.get({ plain: true }).id
-  );
+    // serialize genre search data and get genre ids
+    const genreIdsArr = genreSearchResult.map(
+      (genre) => genre.get({ plain: true }).id
+    );
 
-  // search books
-  const bookSearchResult = await Book.findAll({
-    where: {
-      [Op.or]: [
-        { title: { [Op.like]: `%${query}%` } },
-        { description: { [Op.like]: `%${query}%` } },
-        { author_id: { [Op.in]: authorIdsArr } },
-        { genre_id: { [Op.in]: genreIdsArr } },
-      ],
-    },
-  });
+    // search books
+    const bookSearchResult = await Book.findAll({
+      where: {
+        [Op.or]: [
+          { title: { [Op.like]: `%${query}%` } },
+          { description: { [Op.like]: `%${query}%` } },
+          { author_id: { [Op.in]: authorIdsArr } },
+          { genre_id: { [Op.in]: genreIdsArr } },
+        ],
+      },
+    });
 
-  // serialize each search result
-  const books = bookSearchResult.map((book) => book.get({ plain: true }));
+    // serialize each search result
+    const books = bookSearchResult.map((book) => book.get({ plain: true }));
 
-  // render search results view with found books
-  res.render("search-results", { query, books });
+    // render search results view with found books
+    res.render("search-results", { query, books });
+    
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+    
 });
 
 router.get("/book/:id", (req, res) => {
